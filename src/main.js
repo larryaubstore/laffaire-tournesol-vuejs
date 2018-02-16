2580c0a5f46dc5360191f86dfec2893c305a92ec
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2018-02-15 22:01:52 -0500

    Ajout d3.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import D3Circular from './components/D3Circular.vue';
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+/*
+new Vue({
+  render: h => h(D3Circular)
+}).$mount('#root');
+*/
+
+
